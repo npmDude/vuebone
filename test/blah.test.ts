106f2f6794e83b2b3415fbf0ad2ff904e652114f
Neil Patrick Mendoza <mendoza@housingjapan.com>
2022-01-16 00:25:23 +0900

    Add model test file

--- test/blah.test.ts
+++
@@ -1,7 +0,0 @@
-import Vuebone from '../src';
-
-describe('blah', () => {
-  it('works', () => {
-    expect(sum(1, 1)).toEqual(2);
-  });
-});
---
+++ test/model.test.ts
@@ -0,0 +1,22 @@
+import { Model } from '../src';
+
+describe('model', () => {
+    class TestModel extends Model { }
+    const testModel = new TestModel();
+
+    it('instance', () => {
+        expect(testModel instanceof Model).toEqual(true);
+    });
+
+    test('url', () => {
+        testModel.urlRoot = '/people';
+
+        expect(testModel.url()).toEqual('/people');
+    });
+
+    test('url_with_id', () => {
+        testModel.set({ id: 1 });
+
+        expect(testModel.url()).toEqual('/people/1');
+    });
+});

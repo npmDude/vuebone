b35ad12e93f05c4835d6ac212c5df6a70d1a8272
Neil Patrick Mendoza <mendoza@housingjapan.com>
2022-01-15 22:03:41 +0900

    Update generic types

--- types/index.d.ts
+++ types/index.d.ts
@@ -4,20 +4,5 @@ type ObjectHash = Record<string, any>;
 type _StringKey<T> = keyof T & string;
 
 interface Parseable {
-    parse?: boolean;
-}
-
-// Collection
-
-interface AddOptions {
-    at?: number;
-    merge?: boolean;
-}
-
-interface CollectionSetOptions extends Parseable {
-    add?: boolean | undefined;
-    remove?: boolean | undefined;
-    merge?: boolean | undefined;
-    at?: number | undefined;
-    sort?: boolean | undefined;
+  parse?: boolean;
 }
